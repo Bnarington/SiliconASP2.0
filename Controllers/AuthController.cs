4602276e75ddc61b95edd09f081fb9bed3c1ffec
Bnarington <36935528+Bnarington@users.noreply.github.com>
2024-03-21 13:40:18 +0100

    Created AuthController.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconASP.Controllers;
+
+public class AuthController : Controller
+{
+    [Route("/signup")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
